72ec993b0b50f08afe7c199d1b90e1fdc41e4706
bill <wcstrickland1@gmail.com>
2021-08-04 23:43:59 -0500

    hand arrow emoji changed to event listener

--- js/arrowChange.js
+++ js/arrowChange.js
@@ -1,11 +1,10 @@
-const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-const arrow = document.getElementById("arrow")
+window.addEventListener("resize", (event) => {
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    const arrow = document.getElementById("arrow")
+    if (vw >= 975) {
+        arrow.innerText = " 👉"
+    } else {
+        arrow.innerText = " ☝️"
+    }
+})
 
-console.log(vw)
-console.log(arrow.innerText)
-
-if (vw >= 975) {
-    arrow.innerText = " 👉"
-} else {
-    arrow.innerText = " ☝️"
-}
